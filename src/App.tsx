1c2ad7a96ec0e9df0fa293ba487873f53de6aa3f
Your Name <136835388+Freyanna010@users.noreply.github.com>
2024-07-31 15:02:28 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import { FC } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Main from "./page/main";
+import {Route, Routes } from "react-router-dom";
+import UserBasket from "./page/userBasket";
+import About from "./page/about";
+
 
 const App: FC =  () => {
   return (
     <div className="app">
       <Header />
       <div className="container-page">
-        <Main />
+               
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/basket" element={<UserBasket />} />
+          <Route path="/about" element={<About />} />
+          //TODO: modal or page
+          {/* <Route path="/basket" element={<Login />} /> */}
+        </Routes>
       </div>
     </div>
   );
