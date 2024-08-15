b8279257721b58a27a1b3dadafd1315f1c55b7cc
Your Name <136835388+Freyanna010@users.noreply.github.com>
2024-08-15 13:29:43 +0300

    add-password-show

--- src/page/login/login.tsx
+++ src/page/login/login.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import classes from "./login.module.css";
-import { FC } from "react";
+import { FC, useState } from "react";
 import {
   Controller,
   FieldValues,
@@ -8,9 +8,22 @@ import {
   useForm,
   useFormState,
 } from "react-hook-form";
-import { TextField } from "@mui/material";
-import { pink } from "@mui/material/colors";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { NameValidation, passwordValidation } from "./validation";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey, pink } from "@mui/material/colors";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[50],
+    },
+    error: {
+      main: pink[500],
+    },
+  },
+});
 
 type Input = {
   name: string;
@@ -24,60 +37,84 @@ const Login: FC = () => {
   const { errors } = useFormState({
     control,
   });
-
   const onSubmit: SubmitHandler<Input> = (data) => {
     console.log(data);
     setValue("name", "");
     setValue("password", "");
   };
 
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (
+      event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+      event.preventDefault();
+    };
+
   return (
-    <div className={classes.form}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="name"
-          rules={NameValidation}
-          render={({ field }) => (
-            <TextField
-              label="name"
-              size="small"
-              variant="outlined"
-              color="primary"
-              fullWidth={true}
-              margin="dense"
-              className={classes.form_input}
-              onChange={(e) => field.onChange(e)}
-              value={field.value}
-              error={!!errors.name?.message}
-              helperText={errors.name?.message}
-            />
-          )}
-        />
-        {/*TODO: настроить темы */}
-        <Controller
-          control={control}
-          name="password"
-          rules={passwordValidation}
-          render={({ field }) => (
-            <TextField
-              label="password"
-              size="small"
-              variant="outlined"
-              color="primary"
-              fullWidth={true}
-              margin="dense"
-              className={classes.form_input}
-              onChange={(e) => field.onChange(e)}
-              value={field.value}
-              error={!!errors.password?.message}
-              helperText={errors.password?.message}
-            />
-          )}
-        />
-        <input type="submit" value="log in" />
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="name"
+            rules={NameValidation}
+            render={({ field }) => (
+              <TextField
+                label="name"
+                // size="small"
+                variant="outlined"
+                color="primary"
+                fullWidth={true}
+                margin="dense"
+                className={classes.form_input}
+                onChange={(e) => field.onChange(e)}
+                value={field.value}
+                error={!!errors.name?.message}
+                helperText={errors.name?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            rules={passwordValidation}
+            render={({ field }) => (
+              <TextField
+                label="password"
+                size="small"
+                variant="outlined"
+                color="primary"
+                fullWidth={true}
+                margin="dense"
+                className={classes.form_input}
+                onChange={(e) => field.onChange(e)}
+                value={field.value}
+                error={!!errors.password?.message}
+                helperText={errors.password?.message}
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            )}
+          />
+          <input type="submit" value="log in" />
+        </form>
+      </div>
+    </ThemeProvider>
   );
 };
 export default observer(Login);
+    
